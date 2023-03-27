5e13ea8cb3fb45b644931f8b34e272da585701f5
Jesus Chavez Becerra <jeschb@outlook.com>
2023-03-27 11:58:04 -0500

    Carga Inicial

---
+++ src/main/java/pe/com/caqp/caqpappportalweb/CaqpAppPortalWebApplication.java
@@ -0,0 +1,13 @@
+package pe.com.caqp.caqpappportalweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaqpAppPortalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaqpAppPortalWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/com/caqp/caqpappportalweb/CaqpAppPortalWebApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.com.caqp.caqpappportalweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaqpAppPortalWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
